186614391a64dd0c3d11b47815af52e2dad095f1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-23 22:06:06 +0200

    update routes

--- petbook/src/App.js
+++ petbook/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { Home } from './components/Home/Home'
@@ -7,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      {/* <Routes>
+          <Route path='/home' element={<Home />} />
+      </Routes> */}
       <Footer />
     </div>
   );
