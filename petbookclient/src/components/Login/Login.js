bc40b03b20527efb221463ccc1e04d4e8154fe96
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-24 12:52:19 +0200

    Update Login.js

--- petbookclient/src/components/Login/Login.js
+++ petbookclient/src/components/Login/Login.js
@@ -1,33 +1,29 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
     return (
         <>
         <h1>Login form</h1>
-        <form action="action_page.php" method="post">
+        <form action="action_page.php" method="post" />
             <div class="imgcontainer">
-                <img src="img_avatar2.png" alt="Avatar" class="avatar">
+                <img src="img_avatar2.png" alt="Avatar" class="avatar" />
             </div>
 
-        <div class="container">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
+            <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required />
 
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required />
 
-        <button type="submit">Login</button>
-    <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-    </div>
+            <button type="submit">Login</button>
+            </div>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-    </form>
-    </>
-    )
+            <div class="container" style={{"background-color":"#f1f1f1"}}>
+                <button type="button" class="cancelbtn">Cancel</button>
+                <span class="psw">Forgot <Link to="#">password?</Link></span>
+            </div>
+            </> 
+       
+    );
 }
