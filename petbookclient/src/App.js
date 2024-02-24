a5c0de55a954e12070894aad91263b5a862c5112
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-24 11:20:36 +0200

    adding Register component to App router

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -2,6 +2,7 @@ import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { Home } from  './components/Home/Home';
 import { Login } from  './components/Login/Login';
+import { Register } from  './components/Register/Register';
 import { Route, Routes} from 'react-router-dom'
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path ='/' element={<Home />} />
         <Route path ='/login' element={<Login />} />
+        <Route path ='/register' element={<Register />} />
       </Routes>
     </div>
     <Footer />
