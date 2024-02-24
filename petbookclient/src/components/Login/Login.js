31be14ad45a5c19d739ca4be20f0a7183085e327
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-24 16:18:45 +0200

    set of Register page

--- petbookclient/src/components/Login/Login.js
+++ petbookclient/src/components/Login/Login.js
@@ -1,31 +1,32 @@
 import { Link } from 'react-router-dom';
-import './Login.css'
 
 export const Login = () => {
     return (
         <>
-        
-        <form className='login-form'>
-        <h1>Login form</h1>
+        <section id = "login-page">
+        <form className='login-form' style={{width:"40%", margin:"80px auto" }}>
+        <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Login form</h1>
             <div className="imgcontainer">
                 <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
             </div>
 
-            <div className="container">
-                <label for="uname"><b>Username</b></label>
+            <div className="container" style={{textAlign:"center", fontFamily:"sans-serif"}}>
+                <label for="uname" style={{display:"block"}}><b>Username</b></label>
                 <input type="text" placeholder="Enter Username" name="uname" required />
 
-                <label for="psw"><b>Password</b></label>
+                <label for="psw" style={{display:"block"}}><b>Password</b></label>
                 <input type="password" placeholder="Enter Password" name="psw" required />
 
-            <button type="submit">Login</button>
+            <button style={{display:"block", margin:"10px auto"}} type="submit">Login</button>
             </div>
 
             <div class="container" style={{"background-color":"#f1f1f1"}}>
-                <button type="button" className="cancelbtn">Cancel</button>
-                <span className="psw">Forgot <Link to="#">password?</Link></span>
+                <p className="registerOptionText" style={{textAlign:"center"}}>
+                    You don't have a profile? ... Come on... Go to the <Link to={"/register"}><b>Register page</b></Link>
+                </p>
             </div>
             </form>
+            </section> 
             </> 
        
     );
--- petbookclient/src/components/Register/Register.js
+++ petbookclient/src/components/Register/Register.js
@@ -1,7 +1,38 @@
-
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
     return (
-        <h1>Registration form</h1>
+        <>
+        <section id = "register-page">
+        <form className='register-form' style={{width:"40%", margin:"80px auto" }}>
+        <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Register form</h1>
+            <div className="imgcontainer">
+                <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
+            </div>
+
+            <div className="container" style={{textAlign:"center", fontFamily:"sans-serif"}}>
+                <label for="uname" style={{display:"block"}}><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required />
+
+                <label for="email" style={{display:"block"}}><b>Email address</b></label>
+                <input type="text" placeholder="Enter email address" name="email" required />
+
+                <label for="psw" style={{display:"block"}}><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required />
+
+                <label for="psw" style={{display:"block"}}><b>Confirm Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required />
+
+            <button style={{display:"block", margin:"10px auto"}} type="submit">Login</button>
+            </div>
+
+            <div class="container" style={{"background-color":"#f1f1f1"}}>
+                <p className="registerOptionText" style={{textAlign:"center"}}>
+                    You have a profile? ... Come on... Go to the <Link to={"/login"}><b>Login page</b></Link>
+                </p>
+            </div>
+            </form>
+            </section> 
+            </> 
     )
 }
