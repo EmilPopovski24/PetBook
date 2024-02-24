91086c5de50e3be407060b851f89feb2a24e7257
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-24 11:27:06 +0200

    adding Catalog to app router

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -3,6 +3,7 @@ import { Footer } from './components/Footer/Footer';
 import { Home } from  './components/Home/Home';
 import { Login } from  './components/Login/Login';
 import { Register } from  './components/Register/Register';
+import { Catalog } from  './components/Catalog/Catalog';
 import { Route, Routes} from 'react-router-dom'
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
         <Route path ='/' element={<Home />} />
         <Route path ='/login' element={<Login />} />
         <Route path ='/register' element={<Register />} />
+        <Route path ='/catalog' element={<Catalog />} />
       </Routes>
     </div>
     <Footer />
