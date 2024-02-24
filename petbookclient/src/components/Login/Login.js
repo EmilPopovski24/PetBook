281a7826abc868f131887117ed69bbec0c7bc501
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-24 14:28:00 +0200

    update Login page

--- petbookclient/src/components/Login/Login.js
+++ petbookclient/src/components/Login/Login.js
@@ -5,13 +5,13 @@ export const Login = () => {
     return (
         <>
         
-        <form className='login-form' />
+        <form className='login-form'>
         <h1>Login form</h1>
-            <div class="imgcontainer">
-                <img src="img_avatar2.png" alt="Avatar" class="avatar" />
+            <div className="imgcontainer">
+                <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
             </div>
 
-            <div class="container">
+            <div className="container">
                 <label for="uname"><b>Username</b></label>
                 <input type="text" placeholder="Enter Username" name="uname" required />
 
@@ -22,9 +22,10 @@ export const Login = () => {
             </div>
 
             <div class="container" style={{"background-color":"#f1f1f1"}}>
-                <button type="button" class="cancelbtn">Cancel</button>
-                <span class="psw">Forgot <Link to="#">password?</Link></span>
+                <button type="button" className="cancelbtn">Cancel</button>
+                <span className="psw">Forgot <Link to="#">password?</Link></span>
             </div>
+            </form>
             </> 
        
     );
