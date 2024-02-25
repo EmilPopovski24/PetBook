870ea0b2ba0422f2838aefa3c62ed8860c0e5168
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-25 13:13:47 +0200

    Update useForm.js

--- petbookclient/src/hooks/useForm.js
+++ petbookclient/src/hooks/useForm.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
 
-export const useForm = () => {
-    const [values, setValues] = useState(initialValues, onSubmitHandler);
+export const useForm = (initialValues, onSubmitHandler) => {
+    const [values, setValues] = useState(initialValues);
 
     const changeHandler = (e) => {
     setValues(state =>({...state, [e.target.name]: e.target.value}))
