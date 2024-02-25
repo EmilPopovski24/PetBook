ba28ed890d936909cc3136ce881d79fd9e36d044
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-25 13:02:21 +0200

    Update Login.js

--- petbookclient/src/components/Login/Login.js
+++ petbookclient/src/components/Login/Login.js
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm'
+import { useContext } from 'react';
+
+
 
 export const Login = () => {
+
+    const { onLoginSubmit } = useContext(AuthContext);
+
     return (
         <>
         <section id = "login-page">
-        <form className='login-form' style={{width:"40%", margin:"80px auto" }}>
+        <form className='login-form' onSubmit={onSubmit} style={{width:"40%", margin:"80px auto" }}>
         <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Login form</h1>
             <div className="imgcontainer">
                 <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
