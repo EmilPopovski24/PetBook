82078ba304f5eb928f0f7c3c41c25acc9dd6dce1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-25 13:12:46 +0200

    Update authService.js

--- petbookclient/src/services/authService.js
+++ petbookclient/src/services/authService.js
@@ -1,8 +1,5 @@
 import * as request from './requester';
 
-const baseUrl = 'http://localhost:3000/'
+const baseUrl = 'http://localhost:3000/users'
 
-export const login = (loginData) => {
-    const res = request.post(`${baseUrl}/users/login`)
-    return res
-}
+export const login = (loginData) => request.post(`${baseUrl}/login`);
