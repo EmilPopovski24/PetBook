1b9d7f9e1b112a25bcd0b45c0f81e7efb8bd9aca
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-26 19:18:17 +0200

    header update

--- petbookclient/src/components/Header/Header.js
+++ petbookclient/src/components/Header/Header.js
@@ -1,19 +1,20 @@
-import './Header.css'
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return (
         <>
         <ul className="navbar">
-            <div className='logo-div'>
-                <li className='logo'><a href="/">PetBook</a></li>
-            </div>
-                <li className='header-links'><a href="/">Home</a></li>
-                <li className='header-links'><a href="/login">Login</a></li>
-                <li className='header-links'><a href="/register">Register</a></li>
-                <li className='header-links'><a href="/logout">Logout</a></li>
-                <li className='header-links'><a href="/addpet">Add Your Pet</a></li>
-                <li className='header-links'><a href="/catalog">Catalog</a></li>
-                <li className='header-links'><a href="/about">About</a></li>
+            {/* <div className='logo-div'> */}
+                <li className='logo'><Link to="/">PetBook</Link></li>
+            {/* </div> */}
+                <li className='header-links'><Link to="/">Home</Link></li>
+                <li className='header-links'><Link to="/login">Login</Link></li>
+                <li className='header-links'><Link to="/register">Register</Link></li>
+                <li className='header-links'><Link to="/logout">Logout</Link></li>
+                <li className='header-links'><Link to="/addpet">Add Your Pet</Link></li>
+                <li className='header-links'><Link to="/catalog">Catalog</Link></li>
+                <li className='header-links'><Link to="/about">About</Link></li>
         </ul>
         </>
     )
