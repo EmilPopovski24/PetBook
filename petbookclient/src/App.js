fbb2d1924570a85db2a2e78a516a22fd0b7272c1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-27 19:02:54 +0200

    create petService

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -20,11 +20,13 @@ import { authServiceFactory } from './services/authService';
 function App() {
   const navigate = useNavigate();
   const [auth, setAuth] = useState({});
+  const [pets, setPets] = useState([]);
   const authService = authServiceFactory(auth.accessToken)
 
   const onLoginSubmit = async (data) => {
     const result = await authService.login(data);
     setAuth(result)
+    console.log(result)
     navigate('/catalog')
   };
 
