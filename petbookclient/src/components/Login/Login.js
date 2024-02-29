b6fc3d0f11a7f73dc6485110693f007d8bcce3ca
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-02-29 18:49:07 +0200

    Update Login.js

--- petbookclient/src/components/Login/Login.js
+++ petbookclient/src/components/Login/Login.js
@@ -24,17 +24,14 @@ export const Login = () => {
             <div className="imgcontainer">
                 <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
             </div>
-
             <div className="container" style={{textAlign:"center", fontFamily:"sans-serif"}}>
                 <label htmlFor="email" style={{display:"block"}}><b>Email</b></label>
                 <input type="email" placeholder="Enter Email" id="email" name={LoginKeys.Email} value={values[LoginKeys.Email]} onChange={changeHandler} />
 
                 <label htmlFor="password" style={{display:"block"}}><b>Password</b></label>
                 <input type="password" placeholder="Enter Password" id="password" name={LoginKeys.Password} value={values[LoginKeys.Password]} onChange={changeHandler} />
-
             <button style={{display:"block", margin:"10px auto"}} type="submit">Login</button>
             </div>
-
             <div className="container" style={{"backgroundColor":"#f1f1f1"}}>
                 <p className="registerOptionText" style={{textAlign:"center"}}>
                     You don't have a profile? ... Come on... Go to the <Link to={"/register"}><b>Register page</b></Link>
