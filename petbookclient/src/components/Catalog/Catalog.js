9cd50d631589f2ca6f2fe722872b95908b289fbf
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-01 18:34:22 +0200

    Update Catalog.js

--- petbookclient/src/components/Catalog/Catalog.js
+++ petbookclient/src/components/Catalog/Catalog.js
@@ -4,9 +4,14 @@ export const Catalog = ({
     pets
 }) => {
     return (
+        <>
         <div id='catalog-page'>
         <h1>Pets</h1>
-        {pets.map(x=> <PetItem key={x._id} {...x} />)}
+            {pets.map(x=> <PetItem key={x._id} {...x} />)}
+            {pets.length === 0 && (
+                <h1>No pet accounts for now</h1>
+            )}
         </div>
+        </>
     )
 }
