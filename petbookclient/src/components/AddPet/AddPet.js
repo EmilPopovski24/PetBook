66adcef290aa9ecf34a776f8e1cd3ae372d08917
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-01 18:39:53 +0200

    Update AddPet.js

--- petbookclient/src/components/AddPet/AddPet.js
+++ petbookclient/src/components/AddPet/AddPet.js
@@ -5,16 +5,16 @@ export const AddPet = () => {
         <>
         <h1>Add Your Pet</h1>
             <section id ="create-page">
-                <form id="create" onSubmit={onSubmit} method="POST">
+                <form id="create"  method="POST">
                     <label htmlFor="task-title">Task Title</label>
-                    <input  value={values.title} onChange={changeHandler}  type="text" id="title" name="title" placeholder="Task name..." />
+                    <input type="text" id="title" name="title" placeholder="Task name..." />
                     <hr />
                     <label htmlFor="task-descrition">Description</label>
-                    <input value={values.description} onChange={changeHandler} type="text" id="description" name="description" placeholder="Task description..." />
+                    <input type="text" id="description" name="description" placeholder="Task description..." />
                     <hr />
                     <label htmlFor="task-date">Due Date </label>
                     <br />
-                    <input value={values.date} onChange={changeHandler} type="date" id="date" name="date" placeholder="Task name..." />
+                    <input type="date" id="date" name="date" placeholder="Task name..." />
                     <hr />
                     <button style={{background:"blue", border:"none"}} type="submit" className="btn btn-primary">Create</button>
                 </form>
