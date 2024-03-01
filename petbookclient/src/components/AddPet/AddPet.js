33bd61485cfd525e45bd6e6e78a31e5cae9907e7
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-01 18:44:03 +0200

    Update AddPet.js

--- petbookclient/src/components/AddPet/AddPet.js
+++ petbookclient/src/components/AddPet/AddPet.js
@@ -6,13 +6,19 @@ export const AddPet = () => {
         <h1>Add Your Pet</h1>
             <section id ="create-page">
                 <form id="create"  method="POST">
-                    <label htmlFor="task-title">Task Title</label>
+                    <label htmlFor="task-title">Name</label>
                     <input type="text" id="title" name="title" placeholder="Task name..." />
                     <hr />
-                    <label htmlFor="task-descrition">Description</label>
+                    <label htmlFor="task-title">Type</label>
+                    <input type="text" id="title" name="title" placeholder="Task name..." />
+                    <hr />
+                    <label htmlFor="task-title">Breed</label>
+                    <input type="text" id="title" name="title" placeholder="Task name..." />
+                    <hr />
+                    <label htmlFor="task-descrition">Age</label>
                     <input type="text" id="description" name="description" placeholder="Task description..." />
                     <hr />
-                    <label htmlFor="task-date">Due Date </label>
+                    <label htmlFor="task-date">Color</label>
                     <br />
                     <input type="date" id="date" name="date" placeholder="Task name..." />
                     <hr />
