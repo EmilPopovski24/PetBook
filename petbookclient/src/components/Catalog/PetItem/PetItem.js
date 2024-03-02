9459588779ec0bbdef5c76d82d742a876cc046c1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-02 12:58:52 +0200

    Update PetItem.js

--- petbookclient/src/components/Catalog/PetItem/PetItem.js
+++ petbookclient/src/components/Catalog/PetItem/PetItem.js
@@ -14,7 +14,7 @@ export const PetItem = ({
             <h3>Breed: {breed}</h3>
             <h3>Age: {age}</h3>
             <h3>Color: {color}</h3>
-           <br />
+           <hr />
         </>
     )
 }
