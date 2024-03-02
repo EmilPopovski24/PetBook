c3c44bf89121c6802f33d5bceaae332dda98d85e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-02 12:30:30 +0200

    Update AddPet.js

--- petbookclient/src/components/AddPet/AddPet.js
+++ petbookclient/src/components/AddPet/AddPet.js
@@ -20,20 +20,20 @@ export const AddPet = ({
             <section id ="create-page">
                 <form id="create" onSubmit={onSubmit}  method="POST">
                     <label htmlFor="task-title">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Pet name..." className='parameters'/>
+                    <input type="text" id="name" name="name" values={values.name} onChange={changeHandler} placeholder="Pet name..." className='parameters'/>
                     <hr />
                     <label htmlFor="type-title">Type</label>
-                    <input type="text" id="type" name="type" placeholder="Type..." className='parameters' />
+                    <input type="text" id="type" name="type" values={values.type} onChange={changeHandler} placeholder="Type..." className='parameters' />
                     <hr />
                     <label htmlFor="breed-title">Breed</label>
-                    <input type="text" id="breed" name="breed" placeholder="Breed..." className='parameters' />
+                    <input type="text" id="breed" name="breed" values={values.breed} onChange={changeHandler} placeholder="Breed..." className='parameters' />
                     <hr />
                     <label htmlFor="age">Age</label>
-                    <input type="text" id="age" name="age" placeholder="Age..." className='parameters' />
+                    <input type="text" id="age" name="age" values={values.age} onChange={changeHandler} placeholder="Age..." className='parameters' />
                     <hr />
                     <label htmlFor="color">Color</label>
                     <br />
-                    <input type="text" id="color" name="color" placeholder="Color..." className='parameters' />
+                    <input type="text" id="color" name="color" values={values.color}  onChange={changeHandler} placeholder="Color..." className='parameters' />
                     <hr />
                     <button type="submit" className="addPetBtn">Add Your Pet</button>
                 </form>
