54694868d5d1ce8518f14713531ded1a8972dcb4
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-03 09:28:53 +0200

    Update PetAccount.js

--- petbookclient/src/components/PetAccount/PetAccount.js
+++ petbookclient/src/components/PetAccount/PetAccount.js
@@ -1,27 +1,20 @@
-import { useEffect } from "react";
 import { useService } from '../../hooks/useService'
 import { petServiceFactory } from "../../services/petService";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 
-export const PetAccount = ({
-    name,
-    type,
-    breed,
-    age, 
-    color
-}) => {
-    const { taskId } = useParams();
-    const petService = useService(petServiceFactory)
 
-    useEffect(()=> {
-        petService.getOne(taskId)
-            
-    },[taskId]);
+export const PetAccount = () => {
+    const { taskId } = useParams({});//learn more about useParams
+    const petService = useService(petServiceFactory);
+    const [task, setTask] = useState({});
+    
+   
     return(
         <>
         <h1>PetAccount</h1>
-        <h1>{name}, {type}, {breed}, {age}, {color}</h1>
+        <h1>{values.name}, {values.type}, {values.breed}, {values.age}, {values.color}</h1>
 
         </>
     )
