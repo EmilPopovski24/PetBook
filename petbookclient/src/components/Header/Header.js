0e8bb2a2d8d3c9f709ca8f3d095c3c9f7687b6a3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-05 19:40:59 +0200

    Update Header.js

--- petbookclient/src/components/Header/Header.js
+++ petbookclient/src/components/Header/Header.js
@@ -1,20 +1,34 @@
+import { useContext } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const Header = () => {
+    
+    const { isAuthenticated, username } = useContext(AuthContext);
+
     return (
         <>
         <ul className="navbar">
-            {/* <div className='logo-div'> */}
                 <li className='header-links' id="logo"><Link to="/">PetBook</Link></li>
-            {/* </div> */}
                 <li className='header-links'><Link to="/">Home</Link></li>
-                <li className='header-links'><Link to="/login">Login</Link></li>
-                <li className='header-links'><Link to="/register">Register</Link></li>
-                <li className='header-links'><Link to="/logout">Logout</Link></li>
-                <li className='header-links'><Link to="/addpet">Add Your Pet</Link></li>
-                <li className='header-links'><Link to="/catalog">Catalog</Link></li>
-                <li className='header-links'><Link to="/about">About</Link></li>
+            {!isAuthenticated && (
+                <div className='authenticated'>
+                    <li className='header-links'><Link to="/login">Login</Link></li>
+                    <li className='header-links'><Link to="/register">Register</Link></li>
+                </div>
+            )}
+            {isAuthenticated && (
+                <div>
+                    <li className='header-links'><Link to="/addpet">Add Your Pet</Link></li>
+                    <li className='header-links'><Link to="/catalog">Catalog</Link></li>
+                    <li className='header-links'><Link to="/logout">Logout</Link></li>
+                </div>
+            )}                 
+            <li className='header-links'><Link to="/about">About</Link></li>
+            {isAuthenticated && (
+                <h5>Hello, {username}</h5>
+            )}
         </ul>
         </>
     )
