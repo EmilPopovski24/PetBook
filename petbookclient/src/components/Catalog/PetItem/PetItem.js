91f307a134f2bb92b5308abe043a91bfaec88209
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-06 21:02:53 +0200

    Update PetItem.js

--- petbookclient/src/components/Catalog/PetItem/PetItem.js
+++ petbookclient/src/components/Catalog/PetItem/PetItem.js
@@ -13,10 +13,12 @@ export const PetItem = ({
         <>
         <div className='pet-item'>
             <h1>{name}</h1>
+            <ul>
             <h3>Type: {type}</h3>
             <h3>Breed: {breed}</h3>
             <h3>Age: {age}</h3>
             <h3>Color: {color}</h3>
+            </ul>
             {/* <button className="account-buttons">Edit</button>
             <button className="account-buttons">Delete</button> */}
             <button className="account-buttons"><Link to={`/catalog/${_id}`}>Visit Account</Link></button>
