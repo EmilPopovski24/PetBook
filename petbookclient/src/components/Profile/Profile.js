ee9e1785c38eff5691194dac1f3f34b960cb7a78
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-08 17:35:38 +0200

    Update Profile.js

--- petbookclient/src/components/Profile/Profile.js
+++ petbookclient/src/components/Profile/Profile.js
@@ -1,8 +1,11 @@
-import { useContext } from "react"
-import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { PetItem } from "../Catalog/PetItem/PetItem";
 
 
-export const Profile = () => {
+export const Profile = ({
+    pets
+}) => {
 
     const user = useContext(AuthContext)
 
@@ -15,6 +18,11 @@ export const Profile = () => {
                 <li>Email: {user.userEmail}</li>
             </ul>
         <h3>My pets</h3>
+        {pets.map(x=> <PetItem key={x._ownerId} {...x} />)}
+            {pets.length === 0 && (
+                <h1>No pet accounts for now</h1>
+            )}
+
         </>
     )
 }
