cb2816508a1ea1d6937fba5ee0617f031418f225
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-08 17:18:33 +0200

    Update Profile.js

--- petbookclient/src/components/Profile/Profile.js
+++ petbookclient/src/components/Profile/Profile.js
@@ -1,10 +1,20 @@
 
 
-export const Profile = () => {
+export const Profile = ({
+    email,
+    username
+}) => {
+    
+
     return(
         <>
         <h1>MyProfile</h1>
         <h3>Personal Info</h3> 
+            <ul>
+                <li>Username: {username}</li>
+                <li>Email: {email}</li>
+
+            </ul>
         </>
     )
 }
