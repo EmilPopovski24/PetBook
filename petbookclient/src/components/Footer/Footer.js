38f130789735e6a4afc1d50633f77454be5d5102
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-09 18:50:01 +0200

    Update Footer.js

--- petbookclient/src/components/Footer/Footer.js
+++ petbookclient/src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ export const Footer = () => {
     return (
         <>
         <ul className="footer">
+            
             <h5 className='footer-logo'>© 2024 PetBook</h5>
             <li className='footer-links'><a href="/terms">Terms and Conditions</a></li>
             <li className='footer-links'><a href="/faq">FAQ</a></li>
