c7d754c777b1007ee9c7f859587c000029e6bdb6
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-09 19:01:53 +0200

    Update Home.js

--- petbookclient/src/components/Home/Home.js
+++ petbookclient/src/components/Home/Home.js
@@ -6,6 +6,6 @@ export const Home = () => {
             <div className='img-box'>
                 <img alt ="coverphoto" src='https://www.onepointmedical.com.au/wp-content/uploads/2015/12/gp-melbourne-189974771-1500x1066.jpg' />
             </div>
-        </>
+        </> 
     )
 }
