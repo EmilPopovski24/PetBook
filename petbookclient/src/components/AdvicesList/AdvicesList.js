f806d5a07b26c42064159418e49b46d82e975477
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-10 16:05:56 +0200

    Update AdvicesList.js

--- petbookclient/src/components/AdvicesList/AdvicesList.js
+++ petbookclient/src/components/AdvicesList/AdvicesList.js
@@ -6,7 +6,7 @@ export const AdvicesList = () => {
        <h1>AdvicesList</h1>       
        <h3>Post your problem</h3>
        <form action="" className="problem-form">
-        
+        <textarea name="problem" id="problem" cols="30" rows="10"></textarea>
        </form>
        </>
     )
