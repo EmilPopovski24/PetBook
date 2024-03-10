084467c731bb35b9877582a283b9162f195f194c
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-10 16:04:38 +0200

    Update About.js

--- petbookclient/src/components/About/About.js
+++ petbookclient/src/components/About/About.js
@@ -2,6 +2,11 @@
 
 export const About = () => {
     return (
+        <>
         <h1>About</h1>
+        <p>This is the place where you can find everything about your pet, 
+            share your happiest moments with other pet owners
+        </p>
+        </>
     )
 }
