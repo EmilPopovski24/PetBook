fd374ac6c07f58c586292b17bd7400342f8e08ec
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-11 18:38:12 +0200

    update Catalog view

--- petbookclient/src/components/Catalog/PetItem/PetItem.js
+++ petbookclient/src/components/Catalog/PetItem/PetItem.js
@@ -14,13 +14,13 @@ export const PetItem = ({
         <>
         <div className='pet-item'>
             <h1>{name}</h1>
-            <img src={imageUrl} alt={name} />
-            <ul>
+            <img src={imageUrl} alt={name} className='petitem-pic'/>
+            {/* <ul>
                 <li><h3>Type: {type}</h3></li>
                 <li><h3>Breed: {breed}</h3></li>
                 <li><h3>Age: {age}</h3></li>
                 <li><h3>Color: {color}</h3></li>
-            </ul>
+            </ul> */}
             <button className="account-buttons"><Link to={`/catalog/${_id}`}>Visit Account</Link></button>
            <hr />
         </div>
