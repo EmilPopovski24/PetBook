0bb622d5a6af4064b31c93b46289169ff3834d37
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-15 21:28:16 +0200

    Update Login.js

--- petbookclient/src/components/Login/Login.js
+++ petbookclient/src/components/Login/Login.js
@@ -22,7 +22,7 @@ export const Login = () => {
         <form className='login-form' method="POST" onSubmit={onSubmit} style={{width:"40%", margin:"80px auto" }}>
         <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Login form</h1>
             <div className="imgcontainer">
-                <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
+            <img src="https://fetchpetcare.com/wp-content/uploads/2016/11/dreamstime_xxl_87694876-1024x293.jpg" alt="Avatar" className="avatar" />
             </div>
             <div className="container" style={{textAlign:"center", fontFamily:"sans-serif"}}>
                 <label htmlFor="email" style={{display:"block"}}><b>Email</b></label>
