0214a78abd332c5d1f668a1d24cc9b34f1efc018
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-16 17:47:28 +0200

    Update About.js

--- petbookclient/src/components/About/About.js
+++ petbookclient/src/components/About/About.js
@@ -3,10 +3,8 @@
 export const About = () => {
     return (
         <>
-        <h1>About PetBook</h1>
-        <p>This is the place where you can find everything about your pet, 
-            share your happiest moments with other pet owners
-        </p>
+        <h1 style={{textAlign:"center"}}>About PetBook</h1>
+        <p style={{textAlign:"center"}}>This is the place where you can find everything about your pet, share your happiest moments with other pet owners</p>
         </>
     )
 }
