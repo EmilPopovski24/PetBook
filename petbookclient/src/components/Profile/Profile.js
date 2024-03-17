46c96a8a1355184f195a5101b0d6cf90a88625b9
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-17 14:03:12 +0200

    Update Profile.js

--- petbookclient/src/components/Profile/Profile.js
+++ petbookclient/src/components/Profile/Profile.js
@@ -11,53 +11,25 @@ export const Profile = ({
 }) => {
 
     const user = useContext(AuthContext);
-    // const authService = useService(authServiceFactory);
-    // console.log(pets)
-    // const isPetOwner = user.userId === pets
-    
-    // const onAddPhoto = async(photo) => {
-    //     const result = await authService.addPhoto(photo)
-    //     console.log(photo);
-    //     console.log(result)
-    // }
+
 
     return(
         <>
-        {/* <h1>MyProfile</h1>
-        <h3>Personal Info</h3> 
-            <ul>
-                <li>Username: {user.username}</li>
-                <li>Email: {user.userEmail}</li>
-            </ul>
-        <h3>My pets</h3>
-        {pets.map(x=> <ProfilePet key={x._ownerId} {...x} />)}
-            {pets.length === 0 && (
-                <h1>No pet accounts for now</h1>
-            )} */}
+        
             <div className="card">   
-                {/* <form action="/action_page.php" onSubmit={onAddPhoto}>
-                    <p>Add your profile photo</p>
-                    <label for="img">Select image:</label>
-                    <input type="file" id="img" name="img" accept="image/*" />
-                    <input type="submit"/>
-                </form> */}
-                {/* <img src="img.jpg" alt="{user.username}" className="profile-image" style={{width:"100%"}} /> */}
+                
                 <h3>Personal Info</h3> 
                     <ul>
                         <li>Username: {user.username}</li>
                         <li>Email: {user.userEmail}</li>
                     </ul>         
-                {/* <p className="title">CEO & Founder, Example</p>
-                <p>Harvard University</p> */}
+               
                 <h3>My pets</h3>
                     {pets.map(x=> <ProfilePet key={x._id} {...x} />)}
                     {pets.length === 0 && (
                     <h1>No pet accounts for now</h1>
                 )}
-                {/* <Link to={"/"}><i class="fa fa-dribbble">ui</i></Link>
-                <Link to="/"><i class="fa fa-twitter">ui</i></Link>
-                <Link to="/"><i class="fa fa-linkedin">ui</i></Link>
-                <Link to="/"><i class="fa fa-facebook">ui</i></Link> */}
+                
             </div>
         </>
     )
