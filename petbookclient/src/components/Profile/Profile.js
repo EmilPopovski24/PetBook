18572e541262edfe4c35111f56c35ba473ddb6c0
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-17 15:11:00 +0200

    adding profile picture to Profile.js

--- petbookclient/src/components/Profile/Profile.js
+++ petbookclient/src/components/Profile/Profile.js
@@ -1,9 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-// import { PetItem } from "../Catalog/PetItem/PetItem";
 import { ProfilePet } from "./ProfilePet/ProfilePet";
-import { authServiceFactory } from "../../services/authService";
-import { useService } from "../../hooks/useService";
 import "./Profile.css"
 
 export const Profile = ({
@@ -16,6 +13,7 @@ export const Profile = ({
         <>
             <div className="card">   
                 <h3>Personal Info</h3> 
+                <img src="" alt="profile-pic" className="profile-pic"/>
                     <ul>
                         <li>Username: {user.username}</li>
                         <li>Email: {user.userEmail}</li>
