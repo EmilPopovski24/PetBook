2be112dcbb19cddcc71d7f80188901a49bd8aefb
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-19 20:02:04 +0200

    Update profileService.js

--- petbookclient/src/services/profileService.js
+++ petbookclient/src/services/profileService.js
@@ -2,3 +2,16 @@ import { requestFactory } from "./requester";
 
 const profileUrl = 'http://localhost:3030/data/profile';
 
+export const profileServiceFactory = (token) => {
+    
+    const request = requestFactory(token);
+
+    const addPhoto = async(photo) => {
+        const result = await request.post(`${profileUrl}/photo`, photo);
+        return result
+    }
+    
+    return {
+       addPhoto
+    }
+}
