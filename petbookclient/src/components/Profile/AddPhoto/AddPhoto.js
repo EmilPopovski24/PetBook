e2fe128a9b4583d62ef3bf53b8424a7b75e2dd94
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-19 19:44:17 +0200

    Update AddPhoto.js

--- petbookclient/src/components/Profile/AddPhoto/AddPhoto.js
+++ petbookclient/src/components/Profile/AddPhoto/AddPhoto.js
@@ -1,15 +1,30 @@
 import { useContext, useState } from "react"
 import { AuthContext } from "../../../contexts/AuthContext";
+import { useForm } from "../../../hooks/useForm";
+import { authServiceFactory } from "../../../services/authService";
+import { useService } from "../../../hooks/useService";
 
 export const AddPhoto = () => {
+
+    const authService = useService(authServiceFactory);
+    const [image, setImage] = useState();
+    const { values, changeHandler, onSubmit } = useForm({
+        imageUrl:''
+    } )
+
+    const onProfilePicSubmit = async(data) => {
+        const result = await authService.addPhoto(data)
+        setImage(result)
+        console.log(result)
+    }
 
     return(
         <>
         <section id = "login-page" className='auth'>
-            <form className='photo-form' method="POST" style={{width:"40%", margin:"80px auto" }}>
+            <form className='photo-form' method="POST" onSubmit={on} style={{width:"40%", margin:"80px auto" }}>
                 <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Add your photo</h1>
                 <label htmlFor="image">Image</label>
-                <input type="imageUrl" alt="UserProfile" id="imageUrl" name="imageUrl" placeholder="Image..." className='parameters' />
+                <input type="imageUrl" alt="UserProfile" id="imageUrl" name="imageUrl" placeholder="Image..." className='parameters' value={values.imageUrl} onChange={changeHandler} />
                 <button type="submit" className="addProfilePic">Add Your Photo</button>
             </form>
             </section> 
