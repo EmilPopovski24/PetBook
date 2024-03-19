98d2b1ccc0879b84f719da94d79aad405a1d4ccb
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-19 19:37:17 +0200

    Update AddPhoto.js

--- petbookclient/src/components/Profile/AddPhoto/AddPhoto.js
+++ petbookclient/src/components/Profile/AddPhoto/AddPhoto.js
@@ -9,7 +9,7 @@ export const AddPhoto = () => {
             <form className='photo-form' method="POST" style={{width:"40%", margin:"80px auto" }}>
                 <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Add your photo</h1>
                 <label htmlFor="image">Image</label>
-                <input type="imageUrl" alt="UserProfile" id="imageUrl" name="imageUrl" v placeholder="Image..." className='parameters' />
+                <input type="imageUrl" alt="UserProfile" id="imageUrl" name="imageUrl" placeholder="Image..." className='parameters' />
                 <button type="submit" className="addProfilePic">Add Your Photo</button>
             </form>
             </section> 
