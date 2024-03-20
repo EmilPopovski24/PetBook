0f8e919d553fc0286e5a14ccc837bc3da3ab150a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-20 15:19:57 +0200

    Update AddPhoto.js

--- petbookclient/src/components/Profile/AddPhoto/AddPhoto.js
+++ petbookclient/src/components/Profile/AddPhoto/AddPhoto.js
@@ -1,24 +1,23 @@
 import { useForm } from "../../../hooks/useForm";
 
-
 export const AddPhoto = ({
-    onProilePicSubmit 
+    onProfilePicSubmit 
 }) => {
 
-        const { values, changeHandler } = useForm({
+    const { values, changeHandler, onSubmit } = useForm({
         imageUrl:''
-    })
+    }, onProfilePicSubmit)
 
         return(
         <>
-        <section id = "login-page" className='auth'>
-            <form className='photo-form' method="POST" onSubmit={onProilePicSubmit } style={{width:"40%", margin:"80px auto" }}>
+        <section id = "add-photo-page" className='auth'>
+            <form className='photo-form' method="POST" onSubmit={onSubmit} style={{width:"40%", margin:"80px auto" }}>
                 <h1 style={{textAlign:"center", fontFamily:"sans-serif"}}>Add your photo</h1>
                 <label htmlFor="image">Image</label>
                 <input type="imageUrl" alt="UserProfile" id="imageUrl" name="imageUrl" placeholder="Image..." className='parameters' values={values.imageUrl} onChange={changeHandler} />
                 <button type="submit" className="addProfilePic">Add Your Photo</button>
             </form>
-            </section> 
+        </section> 
         </>
     )
 }
