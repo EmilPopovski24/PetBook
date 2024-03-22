af341faed3fc5e5a50759c12ca45c85d5cb2b716
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-22 18:19:01 +0200

    update Profile view

--- petbookclient/src/components/Profile/Profile.js
+++ petbookclient/src/components/Profile/Profile.js
@@ -14,12 +14,14 @@ export const Profile = ({
         <>
             <div className="card">   
                 <h3>Personal Info</h3> 
-                <button><Link to={'/profile/addphoto'}>Add your Photo</Link></button>
+                <div className="personal-info">
                 <img src={image.imageUrl} alt="profile-pic" className="profile-pic"/>
+                <button className="add-photo"><Link to={'/profile/addphoto'}>Add your Photo</Link></button>
                     <ul>
                         <li>Username: {user.username}</li>
                         <li>Email: {user.userEmail}</li>
-                    </ul>         
+                    </ul>   
+                </div>     
                 {/* <h3>My pets</h3>
                     {pets.map(x=> <ProfilePet key={x._id} {...x} />)}
                     {pets.length === 0 && (
