2b2f53f24294e3a1bd7aea2b5047a686e20f332f
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-24 17:19:24 +0200

    Update App.js

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -31,26 +31,26 @@ function App() {
   const petService = petServiceFactory(); // auth.accessToken
   const profileService = profileServiceFactory(); //auth.accessToken
 
-  useEffect(() => {
+useEffect(() => {
     petService.getAll()
       .then(result=> {
         setPets(result)
       })
-  }, []);
+}, []);
 
-  const onAddPetSubmit = async(petData) => {
+const onAddPetSubmit = async(petData) => {
     const newPet = await petService.addPet(petData);
     setPets(state => [...state, newPet]);
     navigate('/catalog')
-  };
+};
 
-  const onPetEditSubmit = async(values) => {
+const onPetEditSubmit = async(values) => {
     const result = await petService.edit(values._id, values);
     setPets(state => state.map(x=> x._id === values._id ? result : x)) // to check this
     navigate(`/catalog/${values._id}`);
-  }
+};
 
-  const onProfilePicSubmit = async(data) => {
+const onProfilePicSubmit = async(data) => {
     const result = await profileService.addPhoto(data)
     setImage(result)
     navigate(`/profile`)
@@ -67,12 +67,12 @@ function App() {
         <Route path ='/logout' element={<Logout />} />
         <Route path ='/catalog' element={<Catalog pets={pets} />} />    
         <Route element={<RouteGuard />}>
-          <Route path ='/catalog/:petId' element={<PetAccount />} />
-          <Route path ='/catalog/:petId/edit' element={<EditPet onPetEditSubmit={onPetEditSubmit} />} /> 
-          <Route path ='/addpet' element={<AddPet onAddPetSubmit={onAddPetSubmit} />} />
-          <Route path ='/profile' element={<Profile image={image} />} />
-          <Route path ='/profile/addphoto' element={<AddPhoto onProfilePicSubmit={onProfilePicSubmit} />} />
-          <Route path ='/advices' element={<AdvicesList />} />
+            <Route path ='/catalog/:petId' element={<PetAccount />} />
+            <Route path ='/catalog/:petId/edit' element={<EditPet onPetEditSubmit={onPetEditSubmit} />} /> 
+            <Route path ='/addpet' element={<AddPet onAddPetSubmit={onAddPetSubmit} />} />
+            <Route path ='/profile' element={<Profile image={image} />} />
+            <Route path ='/profile/addphoto' element={<AddPhoto onProfilePicSubmit={onProfilePicSubmit} />} />
+            <Route path ='/advices' element={<AdvicesList />} />
         </Route>
         <Route path ='/about' element={<About />} />
         <Route path ='/terms' element={<Terms />} />
