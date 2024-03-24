226ee65214ea6e0b97d6aff2531d2e6f4dd44a65
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-24 16:06:38 +0200

    Update AuthContext.js

--- petbookclient/src/contexts/AuthContext.js
+++ petbookclient/src/contexts/AuthContext.js
@@ -1,3 +1,13 @@
 import { createContext } from "react"; 
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children
+}) => {
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    )
+}
