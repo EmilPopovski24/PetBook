0a4d560532a30e3c765ec4ac6289dc23fd10f992
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-26 18:40:43 +0200

    Update App.js

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -27,7 +27,7 @@ function App() {
   const navigate = useNavigate();
   const [pets, setPets] = useState([]);
   const [image, setImage] = useState({});
-  const [petOwner, setPetOwner] = useState([]);
+  // const [petOwner, setPetOwner] = useState([]);
   const petService = petServiceFactory(); // auth.accessToken
   const profileService = profileServiceFactory(); //auth.accessToken
 
