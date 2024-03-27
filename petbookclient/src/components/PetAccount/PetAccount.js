e1cc6bc3683e213e88349f1c31e87d7e6e271fe9
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-27 16:30:11 +0200

    Update PetAccount.js

--- petbookclient/src/components/PetAccount/PetAccount.js
+++ petbookclient/src/components/PetAccount/PetAccount.js
@@ -40,7 +40,7 @@ export const PetAccount = ({
                     <li><h3>Breed: {pet.breed}</h3></li>
                     <li><h3>Age: {pet.age}</h3></li>
                     <li><h3>Color: {pet.color}</h3></li>
-                    <li><h3>Owner: {petOwner}</h3></li>
+                    <li><h3>Owner: {pet._ownerId.username}</h3></li>
                 </ul> 
                         <div className='actionsDiv'>
                             <div className='go-back'>
