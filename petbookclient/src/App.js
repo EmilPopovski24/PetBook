6ed0428df6c1f9fc3c7fe7a2213c12acc5e4c9e1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-27 11:43:30 +0200

    Update App.js

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -105,28 +105,28 @@ const contextValues = {
 
   return (
     <AuthContext.Provider value={contextValues}>
-    <Header />
-    <div className="main-content">
-      <Routes>
-        <Route path ='/' element={<Home />} />
-        <Route path ='/login' element={<Login />} />
-        <Route path ='/register' element={<Register />} />
-        <Route path ='/logout' element={<Logout />} />
-        <Route path ='/catalog' element={<Catalog pets={pets} />} />    
-        <Route element={<RouteGuard />}>
-            <Route path ='/catalog/:petId' element={<PetAccount />} />
-            <Route path ='/catalog/:petId/edit' element={<EditPet onPetEditSubmit={onPetEditSubmit} />} /> 
-            <Route path ='/addpet' element={<AddPet onAddPetSubmit={onAddPetSubmit} />} />
-            <Route path ='/profile' element={<Profile image={image} />} />
-            <Route path ='/profile/addphoto' element={<AddPhoto onProfilePicSubmit={onProfilePicSubmit} />} />
-            <Route path ='/advices' element={<AdvicesList />} />
-        </Route>
-        <Route path ='/about' element={<About />} />
-        <Route path ='/terms' element={<Terms />} />
-        <Route path ='/faq' element={<FAQ />} />
-      </Routes>
-    </div>
-    <Footer />
+      <Header />
+      <div className="main-content">
+        <Routes>
+          <Route path ='/' element={<Home />} />
+          <Route path ='/login' element={<Login />} />
+          <Route path ='/register' element={<Register />} />
+          <Route path ='/logout' element={<Logout />} />
+          <Route path ='/catalog' element={<Catalog pets={pets} />} />    
+            <Route element={<RouteGuard />}>
+              <Route path ='/catalog/:petId' element={<PetAccount />} />
+              <Route path ='/catalog/:petId/edit' element={<EditPet onPetEditSubmit={onPetEditSubmit} />} /> 
+              <Route path ='/addpet' element={<AddPet onAddPetSubmit={onAddPetSubmit} />} />
+              <Route path ='/profile' element={<Profile image={image} />} />
+              <Route path ='/profile/addphoto' element={<AddPhoto onProfilePicSubmit={onProfilePicSubmit} />} />
+              <Route path ='/advices' element={<AdvicesList />} />
+            </Route>
+          <Route path ='/about' element={<About />} />
+          <Route path ='/terms' element={<Terms />} />
+          <Route path ='/faq' element={<FAQ />} />
+        </Routes>
+      </div>
+      <Footer />
     </AuthContext.Provider>
   );
 }
