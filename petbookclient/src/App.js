07797a59bd721230392625d1b274e816227492ed
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-27 16:28:18 +0200

    Update App.js

--- petbookclient/src/App.js
+++ petbookclient/src/App.js
@@ -46,7 +46,7 @@ function App() {
         const result = await authService.login(data);
         setAuth(result)
         navigate('/catalog')
-        alert("You are welcome!")
+        alert(`You are welcome, ${data.username}`)
 };    
 
     const onRegisterSubmit = async(values) => {
