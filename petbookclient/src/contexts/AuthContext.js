4daa9a8d2bc832e766d6f8d9e2514a62cec8fd73
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-29 18:18:35 +0200

    Update AuthContext.js

--- petbookclient/src/contexts/AuthContext.js
+++ petbookclient/src/contexts/AuthContext.js
@@ -5,64 +5,64 @@ import { createContext} from "react";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({
-    children
-}) => {
+// export const AuthProvider = ({
+//     children
+// }) => {
 
-    const [auth, setAuth] = useLocalStorage('auth', {});
-    const authService = authServiceFactory(auth.accessToken);
-    const navigate = useNavigate();
+//     const [auth, setAuth] = useLocalStorage('auth', {});
+//     const authService = authServiceFactory(auth.accessToken);
+//     const navigate = useNavigate();
 
-    const onLoginSubmit = async (data) => {
-        const result = await authService.login(data);
-        setAuth(result)
-        navigate('/catalog')
-        alert("You are welcome!")
-    };
+//     const onLoginSubmit = async (data) => {
+//         const result = await authService.login(data);
+//         setAuth(result)
+//         navigate('/catalog')
+//         alert("You are welcome!")
+//     };
     
 
-    const onRegisterSubmit = async(values) => {
-        const { confirmPassword, ...registerData } = values;
-        if(confirmPassword !== registerData.password) {
-          alert("Both passwords do not match!")
-          return;
-        };
+//     const onRegisterSubmit = async(values) => {
+//         const { confirmPassword, ...registerData } = values;
+//         if(confirmPassword !== registerData.password) {
+//           alert("Both passwords do not match!")
+//           return;
+//         };
     
-        try {
-            const result = await authService.register(values);
-            setAuth(result);     
-            alert("Successful registration") 
-            navigate('/catalog')
-        } catch (error) {
-            alert("User with the same details (email or username) already exists!")
-        }
-    };
+//         try {
+//             const result = await authService.register(values);
+//             setAuth(result);     
+//             alert("Successful registration") 
+//             navigate('/catalog')
+//         } catch (error) {
+//             alert("User with the same details (email or username) already exists!")
+//         }
+//     };
 
-    const onLogout = async () => {
-        await authService.logout();
-        setAuth({});
-    };
+//     const onLogout = async () => {
+//         await authService.logout();
+//         setAuth({});
+//     };
 
-    const contextValues = {
-        onLoginSubmit,
-        onRegisterSubmit,
-        onLogout,
-        userId: auth._id,
-        token: auth.accessToken,
-        userEmail: auth.email,
-        username: auth.username,
-        isAuthenticated: !!auth.accessToken //truthy - false and vice versa
-      };
+//     const contextValues = {
+//         onLoginSubmit,
+//         onRegisterSubmit,
+//         onLogout,
+//         userId: auth._id,
+//         token: auth.accessToken,
+//         userEmail: auth.email,
+//         username: auth.username,
+//         isAuthenticated: !!auth.accessToken //truthy - false and vice versa
+//       };
 
-    return (
-        <AuthContext.Provider value={contextValues}>
-            {children}
-        </AuthContext.Provider>
-    )
-};
+//     return (
+//         <AuthContext.Provider value={contextValues}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// };
 
-export const useAuthContext = () => {
-    const context = useContext(AuthContext);
+// export const useAuthContext = () => {
+//     const context = useContext(AuthContext);
 
-    return context;
-}
+//     return context;
+// }
