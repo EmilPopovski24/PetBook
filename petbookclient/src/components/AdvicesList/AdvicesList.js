fb0e4fd1fb1d6388a1de71beb720487b64a8c021
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-30 15:14:43 +0200

    Advices.js update

--- petbookclient/src/components/AdvicesList/AdvicesList.js
+++ petbookclient/src/components/AdvicesList/AdvicesList.js
@@ -1,13 +1,18 @@
-
+import './AdvicesList.css';
+import { useForm } from '../../hooks/useForm';
 
 export const AdvicesList = () => {
+
+    const {values, changeHandler, onSubmit} = useForm({
+        problem: '',
+    }, )
     return(
         <>
-       <h1>AdvicesList</h1>       
-       <h3>Post your problem</h3>
-       <form action="" className="problem-form">
-        <textarea name="problem" id="problem" cols="30" rows="10"></textarea>
-        
+       <h1 className="adviceslist-header">AdvicesList</h1>       
+       <h3 className='advisedlist-post'>Post your problem</h3>
+       <form method="POST" className="problem-form" onSubmit={onSubmit}>
+            <textarea name="problem" id="problem" cols="30" rows="10" value={values.problem} onChange={changeHandler}></textarea>
+            <button className='post-btn' type="submit">Post</button>
        </form>
        </>
     )
