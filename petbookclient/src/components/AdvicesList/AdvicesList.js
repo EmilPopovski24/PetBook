b938134e2c856c2798e504a0a7105c6fb95db2e3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-30 15:17:42 +0200

    update on AdviseList view

--- petbookclient/src/components/AdvicesList/AdvicesList.js
+++ petbookclient/src/components/AdvicesList/AdvicesList.js
@@ -9,7 +9,7 @@ export const AdvicesList = () => {
     return(
         <>
        <h1 className="adviceslist-header">AdvicesList</h1>       
-       <h3 className='advisedlist-post'>Post your problem</h3>
+       <h3 className='adviceslist-post'>Post your problem</h3>
        <form method="POST" className="problem-form" onSubmit={onSubmit}>
             <textarea name="problem" id="problem" cols="30" rows="10" value={values.problem} onChange={changeHandler}></textarea>
             <button className='post-btn' type="submit">Post</button>
