c7ca60a3588031cc1d48c6f293424105fa81bbd8
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-31 11:49:10 +0300

    Update Profile.js

--- petbookclient/src/components/Profile/Profile.js
+++ petbookclient/src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import "./Profile.css"
 
