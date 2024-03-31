e349709ecefad813710cd27edbd76dfe2bc376eb
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-31 13:51:04 +0300

    Update Post.js

--- petbookclient/src/components/AdvicesList/Post/Post.js
+++ petbookclient/src/components/AdvicesList/Post/Post.js
@@ -1,13 +1,28 @@
+import { useParams } from "react-router-dom";
 import "./Post.css";
 
 export const Post = ({
     problem,
-    _ownerId
+    _ownerId, 
+
 }) => {
+
+    const postId = useParams();
+
     return (
         <div className="post">
-            <h3 className="post-author">Author: {_ownerId}</h3>
+            <h3 className="post-author">Author: {postId.username}</h3>
             <p className="post-problem">{problem}</p>
+            <div className="comments-div">
+                <h5>Comments:</h5>
+                <ul className="comments-list"> 
+                    {problem.comments && Object.values(problem.comments).map(x=> (
+                        <li key={x._id} className="comment-li">
+                        <p>{postId.username}: {x.comment}</p>
+                        </li>
+                    ))} 
+                </ul>
+            </div>
         </div>
     )
 }
