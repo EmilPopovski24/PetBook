2aff3d830cf07131b76af5efe32a0c355f31699f
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-03-31 13:52:37 +0300

    Update Post.js

--- petbookclient/src/components/AdvicesList/Post/Post.js
+++ petbookclient/src/components/AdvicesList/Post/Post.js
@@ -3,15 +3,12 @@ import "./Post.css";
 
 export const Post = ({
     problem,
-    _ownerId, 
-
+    _ownerId 
 }) => {
-
-    const postId = useParams();
 
     return (
         <div className="post">
-            <h3 className="post-author">Author: {postId.username}</h3>
+            <h3 className="post-author">Author: {_ownerId}</h3>
             <p className="post-problem">{problem}</p>
             <div className="comments-div">
                 <h5>Comments:</h5>
