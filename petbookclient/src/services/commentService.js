9d223c7e42e76dd580bec4a95cc92400b3034550
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-01 18:54:55 +0300

    Update commentService.js

--- petbookclient/src/services/commentService.js
+++ petbookclient/src/services/commentService.js
@@ -6,10 +6,10 @@ export const commentServiceFactory = (token) => {
 
     const request = requestFactory(token);
 
-    const addComment = (postId, comment) => {
-        const result = request.post(`${commentsUrl}`,{postId, comment} )
-        return result;
-    }
+    // const addComment = (postId, comment) => {
+    //     const result = request.post(`${commentsUrl}`,{postId, comment} )
+    //     return result;
+    // }
 
     const getAllComments = async(postId) => {
         return null
