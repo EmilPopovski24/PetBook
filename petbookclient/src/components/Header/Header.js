a2a37582c0da9511c859e9ecc977a0638c8573c1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-03 23:36:50 +0300

    Update Header.js

--- petbookclient/src/components/Header/Header.js
+++ petbookclient/src/components/Header/Header.js
@@ -13,7 +13,7 @@ export const Header = () => {
                 <li className='header-links' id="logo"><Link to="/">PetBook</Link></li>
                 <li className='header-links'><Link to="/">Home</Link></li>
             {!isAuthenticated && (
-                <div className='authenticateasded'>
+                <div className='authenticated'>
                     <li className='header-links'><Link to="/login">Login</Link></li>
                     <li className='header-links'><Link to="/register">Register</Link></li>
                 </div>
