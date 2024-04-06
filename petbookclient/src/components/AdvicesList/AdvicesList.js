35636492312f030eab664d8702c70a4483e97ed8
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-06 13:00:11 +0300

    Update AdvicesList.js

--- petbookclient/src/components/AdvicesList/AdvicesList.js
+++ petbookclient/src/components/AdvicesList/AdvicesList.js
@@ -1,17 +1,18 @@
 import { Post } from './Post/Post';
 import './AdvicesList.css';
+import { Link } from 'react-router-dom';
 
 export const AdvicesList = ({
     posts,
 }) => {
-
+    console.log(posts)
     return(
         <> 
-       <div id='advices-page'>
+        <div id='advices-page'>
             {posts.length > 0 && (
                 <h1 className="catalog-posts">Please advise</h1>
             )}
-            {posts.map(x=> <Post key={x._id} {...x} onCommentSubmit={onCommentSubmit}/>)}
+            {posts}
             {posts.length === 0 && (
                 <h1 className="catalog-posts">No posts for now</h1>
             )}
