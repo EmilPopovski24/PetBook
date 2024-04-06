141ee204a6aac40b121fda1e2b8dc31824befa91
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-06 15:19:57 +0300

    Update Post.js

--- petbookclient/src/components/AdvicesList/PostItem/Post/Post.js
+++ petbookclient/src/components/AdvicesList/PostItem/Post/Post.js
@@ -1,45 +1,47 @@
-// import { useEffect, useState } from 'react';
-// import { useService } from '../../../../hooks/useService';
-// import { commentServiceFactory } from '../../../../services/commentService';
+
 import { useParams } from 'react-router-dom';
 import './Post.css';
+import { useState } from 'react';
+import { useService } from '../../../../hooks/useService';
+import { commentServiceFactory } from '../../../../services/commentService';
 
 export const Post = ({
-
+    problem,
+    _ownerId
 }) => {
 
-    // const [comment, setComment] = useState('')
-    // const [username, setUsername] = useState('');
-    // const commentService = useService(commentServiceFactory);
+    const [comment, setComment] = useState('');
+    const [username, setUsername] = useState('');
+    const commentService = useService(commentServiceFactory);
     const { postId } = useParams();
 
     // useEffect(() => {
     //     commentService.getAllComments(postId)
     //         .then(result => {
-    //             setComment(result)
+    //             setComments(result)
     //         })
     // }, [postId])
 
-    // const onCommentSubmit = async (e) => {
-    //     e.preventDefault();
-    //     await commentService.addComment({
-    //         postId,
-    //         username,
-    //         comment
-    //     })
+    const onCommentSubmit = async (e) => {
+        e.preventDefault();
+        await commentService.addComment({
+            postId,
+            username,
+            comment
+        })
         
-    //     setUsername('');
-    //     setComment('');
-    // };
+        setUsername('');
+        setComment('');
+    };
 
     return (
         <div className="post-comment">
-            <h3 className="post-author">Author: {postId._ownerId}</h3>
-            <p className="post-problem">{postId.problem}</p>
+            <h3 className="post-author">Author: {_ownerId}</h3>
+            <p className="post-problem">{problem}</p>
             <div className="addComment-div">
-                <form className="addComment-form" >
-                    <input type="text" name="username" placeholder="Your name..."  />
-                    <textarea name="comment" cols="100" rows="2"></textarea>
+                <form className="addComment-form" onSubmit={onCommentSubmit}>
+                    <input type="text" name="username" placeholder="Your name..."  value={username} onChange={(e)=> setUsername(e.target.value)} />
+                    <textarea name="comment" cols="100" rows="2" value={comment} onChange={(e)=> setComment(e.target.value)} ></textarea>
                     <button className='post-btn' type="submit">Add comment</button>
                 </form>
             </div>
