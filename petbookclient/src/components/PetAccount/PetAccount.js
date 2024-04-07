668fb511a88ca45b24b92beba383cd4ecd9b60bf
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-07 11:02:40 +0300

    updae commentService in PetAccount.js

--- petbookclient/src/components/PetAccount/PetAccount.js
+++ petbookclient/src/components/PetAccount/PetAccount.js
@@ -12,6 +12,8 @@ export const PetAccount = () => {
     const { petId } = useParams({});//learn more about useParams
     const [pet, setPet] = useState({});
     const [likes, setLikes] = useState(0);
+    const [petComment, setPetComment] = useState('');
+    const [commentUsername, setCommentUsername] = useState('')
     const petService = useService(petServiceFactory);
     const navigate = useNavigate();
 
@@ -32,6 +34,19 @@ export const PetAccount = () => {
     }
 
     const isOwner = pet._ownerId === userId;
+
+    const onPetCommentSubmit = async (e) => {
+        e.preventDefault();
+        await commentService.addComment({
+            postId,
+            username,
+            comment,
+        })
+        
+        setUsername('');
+        setPetComment('');
+    };
+    
 
     return(
         <>
@@ -58,8 +73,28 @@ export const PetAccount = () => {
                             </div>
                         )}
                     </div>
-                 
+                </div>
+                <div className="post-comment">
+            <h3 className="post-author">Author: {_ownerId}</h3>
+            <p className="post-problem">{problem}</p>
+            <div className="addComment-div">
+                <form className="addComment-form" onSubmit={onPetCommentSubmit}>
+                    <input type="text" id="comment-username" name="username" placeholder="Your name..."  value={username} onChange={(e)=> setUsername(e.target.value)} />
+                    <textarea name="comment" id="comment-text" cols="50" rows="3" value={comment} onChange={(e)=> setComment(e.target.value)} ></textarea>
+                    <button className='post-btn' type="submit">Add comment</button>
+                </form>
             </div>
+            <div className="comments-div">
+                <h5>Comments:</h5>
+                <ul className='comments-ul'>
+                    {comments.map(x=> (
+                        <li key={x._id} className='comment-li'>
+                            <p><b>{x.username}</b>: {x.comment}</p>      
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
         </>
     )
 }
