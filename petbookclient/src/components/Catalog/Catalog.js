deb6ae6bb4e3697042e56684ca4d21697c5dae06
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-21 13:22:54 +0300

    update Catalog responsive view

--- petbookclient/src/components/Catalog/Catalog.js
+++ petbookclient/src/components/Catalog/Catalog.js
@@ -8,16 +8,15 @@ export const Catalog = ({
     console.log(pets)
     return (
         <>
-        
             {pets.length > 0 && (
                 <h1 className="catalog-pets">Pets</h1>
             )}
             <div id='catalog-page'>
-            {pets.map(x=> <PetItem key={x._id} {...x} />)}
+                {pets.map(x=> <PetItem key={x._id} {...x} />)}
+            </div>
             {pets.length === 0 && (
                 <h1 className="catalog-pets">No pet accounts for now</h1>
             )}
-        </div>
         </>
     )
 }
