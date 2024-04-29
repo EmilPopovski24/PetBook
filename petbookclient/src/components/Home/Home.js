341bf4e127eb6e83e4ea7f8f01708b8b254d56be
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-29 18:51:05 +0300

    Update Home.js

--- petbookclient/src/components/Home/Home.js
+++ petbookclient/src/components/Home/Home.js
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <>
             <div className='image-box'>
-                <img alt ="coverphoto" src='https://www.onepointmedical.com.au/wp-content/uploads/2015/12/gp-melbourne-189974771-1500x1066.jpg' />
+                <img alt ="coverphoto" style={{width:"50%"}} src='https://www.onepointmedical.com.au/wp-content/uploads/2015/12/gp-melbourne-189974771-1500x1066.jpg' />
             </div>
         </> 
     )
