c7aff0514b3a5f708aa092667d21aee783735acb
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-04-30 19:02:22 +0300

    Update authService.js

--- petbookclient/src/services/authService.js
+++ petbookclient/src/services/authService.js
@@ -3,7 +3,9 @@ import { requestFactory } from "./requester";
 const userUrl = "http://localhost:3030/users"
 
 export const authServiceFactory = (token) => {
+
     const request = requestFactory(token);
+    
     return {
         login:(loginData) => request.post(`${userUrl}/login`, loginData),
         register: (registerData) =>  request.post(`${userUrl}/register`, registerData),
