5df6c2b11398645a625fbcc7cfc89ebb348c061a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-04 10:27:49 +0300

    Update PetContext.js

---
+++ petbookclient/src/contexts/PetContext.js
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import { petServiceFactory } from "../services/petService";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+
+
+
+
+export const PetProvider = ({
+    auth
+}) => {
+    const petService = petServiceFactory(auth.accessToken);
+    const [pets, setPets] = useLocalStorage('auth', []);
+    const navigate = useNavigate()
+
+    const onAddPetSubmit = async(petData) => {
+        const newPet = await petService.addPet(petData);
+        setPets(state => [...state, newPet]);
+        navigate('/catalog')
+};
+
+    const onPetEditSubmit = async(values) => {
+        const result = await petService.edit(values._id, values);
+        setPets(state => state.map(x => x._id === values._id ? result : x)) // to check this
+        navigate(`/catalog/${values._id}`);
+};
+
+const contextValues = {
+    onAddPetSubmit,
+    onPetEditSubmit
+}
+
+}
