55750dec0bf5c067ce85c23990f7fe7b5b2d9b0b
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-05 12:17:00 +0300

    Update Home.test.js

--- petbookclient/src/components/Home/Home.test.js
+++ petbookclient/src/components/Home/Home.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { Home } from './Home';
 
-test('renders learn react link', () => {
+test('check if header is visible', () => {
   render(<Home />);
-  const h1 = screen.getAllByText("Homepage");
-  expect(h1).toBeInTheDocument();
+  const h1 = screen.findAllByText('Homepage')
+  expect(h1).toBeTruthy();
 });
