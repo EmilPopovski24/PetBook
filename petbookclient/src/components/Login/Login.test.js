57192272ef38ff781a40d70f9a62de6067d9cb1a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-05 17:03:56 +0300

    Update Login.test.js

--- petbookclient/src/components/Login/Login.test.js
+++ petbookclient/src/components/Login/Login.test.js
@@ -1,8 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import { Login } from './Login';
 
-test('check if header is visible', () => {
-    render(<Login />);
-    const { pic }  = screen.findByAltText("Avatar")
-    expect(pic).toBeTruthy();
-  });
