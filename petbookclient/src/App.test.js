3cecedc30d2d6890424241447101d1f8282ae7ed
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-05 11:25:53 +0300

    implement unit testing for header

--- petbookclient/src/App.test.js
+++ petbookclient/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Home from '../src/components/Home/Home';
+import { Header } from './components/Header/Header';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Test App component", () => {
+  it("renders home page with picture", () => {
+    const { logo } = render(<Header />)
+    const headerlogo = screen.getbyText(/PetBook/i);
+    expect(headerlogo).toVeTruthy();
+  });
+})
