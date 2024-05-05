178a3d576d11553947af7264cb24dfb56e6858d5
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-05 11:51:04 +0300

    Update App.test.js

--- petbookclient/src/App.test.js
+++ petbookclient/src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Home from '../src/components/Home/Home';
-import { Header } from './components/Header/Header';
-import { Login } from './components/Login/Login';
+
 
 test('renders learn react link', () => {
   render(<App />);
