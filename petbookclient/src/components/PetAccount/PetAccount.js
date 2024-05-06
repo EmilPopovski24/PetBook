7b68dcacdd1a52b33e547069c1b0e285cf48f170
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-06 15:44:54 +0300

    Update PetAccount.js

--- petbookclient/src/components/PetAccount/PetAccount.js
+++ petbookclient/src/components/PetAccount/PetAccount.js
@@ -35,7 +35,7 @@ export const PetAccount = () => {
         const result = confirm((`Are you sire you want to delete ${pet.name}`))
 
         if(result) {
-            await petService.deleteP(pet._id);
+            await petService.deletePet(pet._id);
         }
         
         navigate("/catalog")
