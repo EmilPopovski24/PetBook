67cd5cac61da4649e460b4ada42927c7441b5297
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-08 18:17:54 +0300

    Update RouteGuard.js

--- petbookclient/src/components/common/RouteGuard.js
+++ petbookclient/src/components/common/RouteGuard.js
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthContext } from "../../contexts/AuthContext";
 
-
 export const RouteGuard = () => {
+
     const { isAuthenticated } = useAuthContext();
 
     if (!isAuthenticated) {
