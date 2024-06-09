b1278ed42e1a5f45534e130f72c0cf5c69c4088e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-09 13:38:45 +0300

    update requester

--- petbookclient/src/services/requester.js
+++ petbookclient/src/services/requester.js
@@ -8,7 +8,7 @@ const request = async (method, token, url, data) => {
                 options.headers = {
                     'content-type':'application/json', 
                 };
-                options.body = JSON.stringify(data);
+                options.body = JSON.parse(data);
             }   
         }
     
