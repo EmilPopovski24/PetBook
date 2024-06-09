8cbe485b8158639d791d57f8b371d0690961645b
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-09 14:38:22 +0300

    Update PetAccount.js

--- petbookclient/src/components/PetAccount/PetAccount.js
+++ petbookclient/src/components/PetAccount/PetAccount.js
@@ -98,8 +98,7 @@ export const PetAccount = () => {
             </div>
             <div className="petComments-div">
                 <h5>Comments:</h5>
-                <ul className='petComments-ul'>
-                    
+                <ul className='petComments-ul'>  
                     {petComments?.map(x=> (
                         <li key={x._id} className='petComment-li'>
                             <p><b>{x.author.username}</b>: {x.petComment}</p>      
