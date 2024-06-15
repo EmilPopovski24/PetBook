638ae791ea3f6da439891c003de30c1a65ffbacf
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-15 10:15:14 +0300

    Update PostItem.js

--- petbookclient/src/components/AdvicesList/PostItem/PostItem.js
+++ petbookclient/src/components/AdvicesList/PostItem/PostItem.js
@@ -4,13 +4,13 @@ import "./PostItem.css";
 export const PostItem = ({
     problem, 
     _ownerId,
-    username,
+    user,
     _id
 }) => {
     
     return(
         <div className='post-item'>
-            <h3>Author:{username}</h3>
+            <h3>Author:{user}</h3>
             <h5>{problem}</h5>
             <button className="account-buttons"><Link to={`/advices/${_id}`}>Assist</Link></button>
             <hr />
