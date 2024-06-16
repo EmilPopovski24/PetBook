3e266b7b69b7aedabe81568a343178b3a897930c
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-06-16 13:36:48 +0300

    update fonts

--- petbookclient/src/components/About/About.js
+++ petbookclient/src/components/About/About.js
@@ -5,10 +5,9 @@ export const About = () => {
         <>
         <h1 style={{textAlign:"center", fontFamily:"Segoe UI', Tahoma, Geneva, Verdana, sans-serif"}}>About PetBook</h1>
         <p style={{textAlign:"center", fontFamily:"Segoe UI', Tahoma, Geneva, Verdana, sans-serif"}}>This is the place where you can find everything about your pet, share your happiest moments with other pet owners.</p>
-        <div className='image-boxa-about'>
+        <div className='image-box'>
                 <img alt ="coverphoto" style={{width:"50%"}} src='https://www.onepointmedical.com.au/wp-content/uploads/2015/12/gp-melbourne-189974771-1500x1066.jpg' />
-            </div>
-
+        </div>
         </>
     )
 }
