6ebeca12b3efa9d02e1ff5a6163b0c9a4063c537
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-04 19:13:18 +0300

    adding Like Me button

--- petbookclient/src/components/PetAccount/PetAccount.js
+++ petbookclient/src/components/PetAccount/PetAccount.js
@@ -67,6 +67,7 @@ export const PetAccount = () => {
                     <li><h3>Age: {pet.age}</h3></li>
                     <li><h3>Color: {pet.color}</h3></li>
                     <li><h3>Owner: {pet.username}</h3></li>
+                    <li><button>Like Me</button></li>
 
                 </ul> 
                     <div className='actionsDiv'>
