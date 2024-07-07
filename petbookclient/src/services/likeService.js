47f0e96e5c2c6e4fc1135113197a85622d72087c
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-07-07 15:42:34 +0300

    Update likeService.js

--- petbookclient/src/services/likeService.js
+++ petbookclient/src/services/likeService.js
@@ -2,7 +2,6 @@ import { requestFactory } from "./requester";
 
 const likesUrl = 'http://localhost:3030/data/likes';
 
-
 export const likeServiceFactory = (token) => {
 
     const request = requestFactory(token);
