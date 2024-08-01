4541535a467d2d5b91cb30e52bedacf0ecb94025
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-08-01 19:33:26 +0300

    test

--- petbookclient/src/components/AddPet/AddPet.js
+++ petbookclient/src/components/AddPet/AddPet.js
@@ -39,6 +39,7 @@ export const AddPet = () => {
                     <input type="text" id="color" name="color" values={values.color}  onChange={changeHandler} placeholder="Color..." className='parameters' />
                     <hr />
                     <label htmlFor="image">Image URL</label>
+                    
                     <input type="imageUrl" alt="PetPicture" id="imageUrl" name="imageUrl" values={values.imageUrl} onChange={changeHandler} placeholder="Image..." className='parameters' />
                     <hr />
                     <button type="submit" className="addPetBtn">Add Your Pet</button>
