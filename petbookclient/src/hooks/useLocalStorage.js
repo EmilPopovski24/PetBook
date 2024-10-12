6d3b1c7f275de94edf9a0a98cef5acc858305c46
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-10-12 21:07:18 +0300

    Update useLocalStorage.js

--- petbookclient/src/hooks/useLocalStorage.js
+++ petbookclient/src/hooks/useLocalStorage.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export const useLocalStorage = (key, initialValue) => {
 
     const [state, setState] = useState(() => {
+        
         const persistedStateSerialized = localStorage.getItem('key');
 
         if (persistedStateSerialized) {
